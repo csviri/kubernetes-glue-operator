cf6e6cf1caa8ace24e26e20a43df9269c8302067
Attila Mészáros <csviri@gmail.com>
2023-11-27 20:08:37 +0100

    minor update
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

--- src/main/java/io/csviri/operator/workflow/Runner.java
+++ src/main/java/io/csviri/operator/workflow/Runner.java
@@ -14,5 +14,6 @@ public class Runner {
     operator.register(new WorkflowReconciler());
     operator.start();
     log.info("Workflow controller started.");
+
   }
 }
--- src/main/java/io/csviri/operator/workflow/customresource/operator/WorkflowOperator.java
+++ src/main/java/io/csviri/operator/workflow/customresource/operator/WorkflowOperator.java
@@ -3,6 +3,8 @@ package io.csviri.operator.workflow.customresource.operator;
 import io.csviri.operator.workflow.customresource.workflow.WorkflowSpec;
 import io.csviri.operator.workflow.customresource.workflow.WorkflowStatus;
 import io.fabric8.kubernetes.client.CustomResource;
+import io.fabric8.kubernetes.model.annotation.ShortNames;
 
+@ShortNames("wo")
 public class WorkflowOperator extends CustomResource<WorkflowSpec, WorkflowStatus> {
 }
